ba51b4266308b6484837abf48a3f74939ab55fcc
kyuwankim <lpnids@gmail.com>
2017-05-11 14:21:30 +0900

    Generic 추가

--- src/com/kyuwan/java/collection/CollectionMain.java
+++ src/com/kyuwan/java/collection/CollectionMain.java
@@ -9,19 +9,26 @@ public class CollectionMain {
 		// 1. List 계열
 		// .add(값) : 값입력
 		// .get(순서) : 값출력 
-		ArrayList list = new ArrayList();
+		ArrayList<Integer> list = new ArrayList<>();
 		int a = 33;
 		
-		list.add(457);
+		list.add(457); // 제네릭을 사용하지 않는 컬렉션은 입력되는 값을 Object로 Casting 한다 
 		list.add(1234);
-		list.add("AAAA");
+		//list.add("AAAA");
+		
+		// 스트링 첫번째 글자 출력하기 
+		System.out.println("ABCDE".substring(0, 1));
+		
 		
 		for(int i = 0 ; i < list.size() ; i++){
 			System.out.println(list.get(i));
 		}
 		
+		// 향상된 for 문
 		for(Object item : list){
-			System.out.println(item);
+			String string = item + "";
+			int number = Integer.parseInt(string);
+			System.out.println(number + 3);
 		}
 		
 		// 2. Map 계열
