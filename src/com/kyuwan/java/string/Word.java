8a8d3dedfbcdbb18b8805071f08fcc46cf1af32b
kyuwankim <lpnids@gmail.com>
2017-05-11 15:32:33 +0900

    String 을 소개하기위한 Word.java 추가

---
+++ src/com/kyuwan/java/string/Word.java
@@ -0,0 +1,14 @@
+package com.kyuwan.java.string;
+
+public class Word {
+	
+	public static void main(String[] args){
+		
+		String name = "æ»≥Á«œººø‰";
+		
+		String secondWorld = name.substring(1, 2);
+		
+		System.out.println(secondWorld);
+	}
+
+}
