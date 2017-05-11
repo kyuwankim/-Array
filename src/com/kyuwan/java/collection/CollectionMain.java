3f164f4085fe98d0b8f0bdae2e5fcf6a3303912a
kyuwankim <lpnids@gmail.com>
2017-05-11 14:09:25 +0900

    ArrayList 추

--- src/com/kyuwan/java/collection/CollectionMain.java
+++ src/com/kyuwan/java/collection/CollectionMain.java
@@ -1,11 +1,22 @@
 package com.kyuwan.java.collection;
 
+import java.util.ArrayList;
+
 public class CollectionMain {
 
 	public static void main(String[] args) {
 
 		// 1. List 계열
+		ArrayList list = new ArrayList();
+		int a = 33;
 		
+		list.add(457);
+		list.add(1234);
+		list.add("AAAA");
+		
+		for(int i = 0 ; i < list.size() ; i++){
+			System.out.println(list.get(i));
+		}
 		
 		// 2. Map 계열
 		
